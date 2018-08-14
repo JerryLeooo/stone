8fd50da9a7cdb86af5d4603ccc994d1aa6784f84
jerryleooo <whilgeek@gmail.com>
2018-08-14 21:02:00 +0800

    test gitconfig

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -6,7 +6,7 @@ from stone.lexer.token import Token
 def test_parser():
     pwd = os.path.abspath(os.path.join(__file__, os.pardir))
 
-    with open("%s/more.stone", "r") as fp:
+    with open("%s/more.stone" % pwd, "r") as fp:
         lexer = Lexer(fp)
         bp = BasicParser()
         while lexer.peek(0) != Token.EOF:
